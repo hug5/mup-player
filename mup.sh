ef5a8b2a57deade7cd34801fdae55b917ab5e344
hug5 <dhub2@gmx.com>
2026-05-26 03:43:04 -0700

    wip

--- mup.sh
+++ mup.sh
@@ -9,6 +9,9 @@
 #---------------------------------------------------------
 
 # This script should not be sourced;
+# Not sourcing runs the program in a new shell;
+# Sourcing runs the program in the current shell;
+#
 
 
 #---------------------------------------------------------
@@ -25,12 +28,12 @@ declare MARG="--audio-display=no --volume=$VOL_LEVEL --loop-playlist --speed=1.0
   # with preview playlist;
 declare STYLE="--border --prompt=: --header=————————————————————————————————"
   # No preview of playlist
-declare PREV='--preview-window=right:40%:wrap'
+declare PREV='--preview-window=right:60%:wrap'
 
 declare SHUFFLE=true      # Shuffle songs: true/false
 declare SEARCH_TYPE="f"   # f=file; d=directory; file is default;
 declare -i COUNT=0
-declare PTYPE=''
+declare PTYPE=''          # Play Type: fuzzy, all, here, playlist
 declare FLAGS=''
 declare -i DEPTH=1
 declare -a ALL_FILES
@@ -99,8 +102,10 @@ function check_longflag_playtype() {
     # This is a bit of a hack
     # --xxx option must be the first option;
     # local STR="$*"
-
     # case $STR in
+
+    # Set PTYPE variable; or play type;
+
     case $FLAGS in
 
       "--all "* | "--all")
@@ -130,13 +135,16 @@ function check_longflag_playtype() {
 
     esac
 
-    # Remove all --xxx flags because it seems to interfere with regular single dash flags;
+    # Remove all --xxx flags because it seems to interfere with
+     # regular single dash flags;
     # FLAGS=$(echo "$FLAGS" | sed 's/--[a-z0-9 ]*//g')
       # [a-z ] : a-z or space; do this to remove --all, --here, etc;
       # But we don't want to remove the -s in, --here -s;
 
-    # Note: This isn't necessary, actually, unless you want to remove multiple --xx flags; can just call "shift" command to shift over to next arguments, instead; so will just do that instead; multiple --xxx flags will just produce error;
-
+    # Note: This isn't necessary, actually, unless you want to
+     # remove multiple --xx flags; can just call "shift" command
+     # to shift over to next arguments, instead; so will just do
+     # that instead; multiple --xxx flags will just produce error;
 }
 
 function check_bad_longflag() {
@@ -149,6 +157,7 @@ function check_bad_longflag() {
 
 function check_shortflags() {
     local OPTIND
+      # OPTIND = index of next argument; not current!
       # Make this a local; the index of the next
       # argument index, not current;
     local LOPTION
@@ -157,7 +166,18 @@ function check_shortflags() {
     local LDEPTH
       # When --fuzzy, get directory DEPTH; default=1
 
-    while getopts ":dsnfhFAHPN:" LOPTION; do  # Loop: Get the next option;
+    # Set variabless:
+     # DEPTH
+     # SHUFFLE
+     # PTYPE (if long version hasn't been yet set)
+     # SEARCH_TYPE
+
+    # Loop: Get the next option;
+    # x: = takes an arguments
+    # no : = does not take argument
+    # : at beginning = silent error on unsupported option
+    # Below, only N takes argument;
+    while getopts ":dsnfhFAHPN:" LOPTION; do
 
         case "$LOPTION" in
 
@@ -185,6 +205,7 @@ function check_shortflags() {
             # exit
             ;;
           N)
+            # $OPTARG = switch argument
             LDEPTH="${OPTARG}"
 
             if [[ "$LDEPTH" -ge 2 ]]; then
@@ -220,7 +241,12 @@ function mup_fzy() {
 
     ALL_FILES=$($fd -t f $AUDIO_TYPE | fzf -m $STYLE | sed -e 's/.*/\"&\"/')
       # AUDIO_TYPE won't work if quotes;
+
+      # fd-find
       # -t f : type files;
+
+      # fzf
+      # -m : multiselect
 
     if [[ -z "$ALL_FILES" ]]; then
         echo "Goodbye."
@@ -259,6 +285,19 @@ function mup_fzyd() {
 
 }
 
+
+# fd-find options:
+  # -t f : filter search by type
+    # f, file : regular file
+    # d, directory : directories
+  # -d d : directory depth; limit to max-depth d
+    # By default, no limit;
+
+# sed : stream editor
+ # -e : perform script operation; can use to do multiple operations
+ # sed -e 's/.*/\"&\"/' : wrap string in quotes
+ # & represents the matched string
+
 function mup_all() {
     ALL_FILES=$($fd -t f $AUDIO_TYPE | sed -e 's/.*/\"&\"/')
 
@@ -278,8 +317,12 @@ function mup_playlist() {
 
     local ISBAT=''
     local ISBATCAT=''
-    ISBAT=$(hash bat &>/dev/null)
-    ISBATCAT=$(hash batcat &>/dev/null)
+    # ISBAT=$(hash bat 2>/dev/null)
+    # ISBATCAT=$(hash batcat 2>/dev/null)
+      # Didn't realize that this never worked!!
+      # hash command doesn't work on my ubuntu
+    ISBAT=$(command -v bat 2>/dev/null)
+    ISBATCAT=$(command -v batcat 2>/dev/null)
       # Check if bat/batcat exists; if so, use bat; else use head for preview;
       # LSP suggestion:
       # https://www.shellcheck.net/wiki/SC2230
@@ -290,28 +333,51 @@ function mup_playlist() {
       # hash <command>
       # do &>/dev/null to silence any errors;
 
+    # // 2026-05-21
+    # Added awk -F/ '{print \$NF}' to print just the song name w/o path;
+    # Putting playlist in PLAYLIST variable;
     if [[ -n $ISBAT ]]; then
-        PLAYLIST=$($fd -t f -e m3u | fzf -m $STYLE "$PREV" --preview='bat --color=always --line-range=:100 {}')
+        # echo "here 1"
+        PLAYLIST=$($fd -t f -e m3u | fzf -m $STYLE "$PREV" --preview="bat -pp --color=always --line-range=:100 {} | awk -F/ '{print \$NF}' ")
     elif [[ -n $ISBATCAT ]]; then
-        PLAYLIST=$($fd -t f -e m3u | fzf -m $STYLE "$PREV" --preview='batcat --color=always --line-range=:100 {}')
+        # echo "here 2"
+        # PLAYLIST=$($fd -t f -e m3u | fzf -m $STYLE "$PREV" --preview "head -n100 {} | awk -F/ '{print \$NF}' ")
+        PLAYLIST=$($fd -t f -e m3u | fzf -m $STYLE "$PREV" --preview="batcat -pp --color=always --line-range=:100 {} | awk -F/ '{print \$NF}' ")
     else
-        PLAYLIST=$($fd -t f -e m3u | fzf -m $STYLE "$PREV" --preview='head -n100 {}' )
+        # echo "here 3"
+        # PLAYLIST=$($fd -t f -e m3u | fzf -m $STYLE "$PREV" --preview='head -n100 {} ' )
+        PLAYLIST=$($fd -t f -e m3u | fzf -m $STYLE "$PREV" --preview="head -n100 {} | awk -F/ '{print \$NF}' ")
     fi
 
 
+    # If no playlist chosen;
     if [[ -z "$PLAYLIST" ]]; then
         echo "Goodbye."
         exit
     fi
 
+
+    # Read the catted playlist:
+    # IFS= = internal field separator; in this case, do not delimit by white space;
+    # read -r : do not respect escape characters;
     while IFS= read -r line; do
         NEW_FILES=$(echo "$line" | sed -e 's/.*/\"&\"/')
-        NEW_FILES+=$'\n' # add new line character
-          # Not sure why I have to add a newline in this case;
+          # wrap in quotes
+        NEW_FILES+=$'\n'
+          # add new line character; the $ ensures it's a real newline,
+           # not just '\n' literal characters;
         ALL_FILES+="${NEW_FILES}"
+
     done < <(while IFS= read -r list; do
             cat "$list"
+            # send cat of playlist content to above read;
         done < <(printf '%s\n' "$PLAYLIST"))
+        # Name of .m3u file, not the contents;
+        # %s : print as string;
+        # Needs to add \n to playlist because 'read' can only
+         # read if it's terminated by newline character;
+        # Since we need a \n, could just use echo as well;
+
 
     # Remove the last blank line; either of these seem to work;
     ALL_FILES=$(echo "$ALL_FILES" | grep .)
@@ -327,6 +393,7 @@ function mup_play() {
     fi
 
     COUNT=$(echo "$ALL_FILES" | wc -l)
+      # wc -l : get # of lines
 
     if [[ "$SHUFFLE" == true ]]; then
 
@@ -344,12 +411,18 @@ function mup_play() {
 }
 
 # Not sure if this will be buggy, but this seems to work;
-# Want to put into a variable becuase need to edit the flags and options; since I'm using --here, --fuzzy flags along with -s, -d; it seems that the built-in bash interprets --playlist, for instance, as an -s flag because there's an s in it; that's not good! So after checking for the --flags, I delete them all from the string;
+# Want to put into a variable becuase need to edit the flags and
+ # options; since I'm using --here, --fuzzy flags along with -s,
+ # -d; it seems that the built-in bash interprets --playlist, for
+ # instance, as an -s flag because there's an s in it; that's not
+ # good! So after checking for the --flags, I delete them all from
+ # the string;
 FLAGS="$*"
+  # "$*" : As a single string
+  # "$@" : As separate distinct strings
 
-# Check for --flags
-# check_longflag_playtype "$*"
-
+# Check for --flags; get PTYPE or Play Type:
+ # fuzzy, all, here, playlist
 check_longflag_playtype "$FLAGS"
 
 # check_shortflags $FLAGS
@@ -361,14 +434,19 @@ check_longflag_playtype "$FLAGS"
   # Using both --long and -s flag types and parsing string myself;
 
 # Only shift if long flag was used:
+# If PTYPE variable was set, then shift;
+# shift discards positional parameters, $1, $2, $3, etc;
+# -n : Not empty
 if [[ -n "$PTYPE" ]]; then
     shift
 fi
 
 
+# Now check short flags:
 check_shortflags $*
 
 
+# If PTYPE not set, then default to fuzzy
 if [[ -z "$PTYPE" ]]; then
     # if PTYPE long flag not used, then set PTYPE default to fuzzy;
     PTYPE="fuzzy"
@@ -376,23 +454,22 @@ fi
 
 
 if [[ $PTYPE == "all" ]]; then
-  mup_all
+    mup_all
 
 elif [[ $PTYPE == "here" ]]; then
-  mup_here
+    mup_here
 
 elif [[ $PTYPE == "fuzzy" ]]; then
-  # If file, then run file function;
-  if [[ "$SEARCH_TYPE" == "f" ]]; then
-      mup_fzy
-  # if -d, directory, then run directory function;
-  else
-      mup_fzyd
-  fi
+    # If file, then run file function;
+    if [[ "$SEARCH_TYPE" == "f" ]]; then
+        mup_fzy
+    # if -d, directory, then run directory function;
+    else
+        mup_fzyd
+    fi
 
 elif [[ $PTYPE == "playlist" ]]; then
-  mup_playlist
-
+    mup_playlist
 fi
 
 # After accumulating song files, play them:
